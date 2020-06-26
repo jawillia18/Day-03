88efd81344585c01a637b708aa833224a5efca7e
jayden williams <jawillia@c5r7s3.wethinkcode.co.za>
2020-06-26 09:34:05 +0200

    ex01 updated

--- ex01/ft_ultimate_ft.c
+++ ex01/ft_ultimate_ft.c
@@ -6,36 +6,42 @@
 /*   By: jawillia <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/06/25 12:58:45 by jawillia          #+#    #+#             */
-/*   Updated: 2020/06/25 17:08:55 by jawillia         ###   ########.fr       */
+/*   Updated: 2020/06/26 09:32:20 by jawillia         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include <stdio.h>
 #include <unistd.h>
 
-void 		ft_ultimate_ft(int *********nbr);
-
+void ft_ultimate_ft(int *********nbr)
+{
+	*********nbr = 42;
+}
 int main()
 {	
-	int a = 42;
-	int nbr = &a;
-	int *nbr1;
-	int **nbr2;
-	int ***nbr3;
-	int ****nbr4;
-	int *****nbr5;
-	int ******nbr6;
-	int *******nbr7;
-	int ********nbr8;
-	int *********nbr;
-	nbr = nbr ;
-	nbr8 = &nbr7;
-	nbr7 = &nbr6;
-	nbr6 = &nbr5;
-	nbr5 = &nbr4;
-	nbr4 = &nbr3;
-	nbr3 = &nbr2;
-	nbr2 = &nbr1;
+	int a;
+	int *ptr;
+					/*int *ptr1;
+					int **ptr2;
+					int ***ptr3;
+					int ****ptr4;
+					int *****ptr5;
+					int ******ptr6;
+					int *******ptr7;
+					int ********ptr8;
+					int *********ptr9;
+					ptr1 = &a;
+					ptr2 = &ptr1;
+					ptr3 = &ptr2;
+					ptr4 = &ptr3;
+					ptr5 = &ptr4;
+					ptr6 = &ptr5;
+					ptr7 = &ptr6;
+					ptr8 = &ptr7;
+					ptr9 = &ptr8;*/
+	a = 12;
+	ptr = &a;
 	printf ("%d",a);
+	ft_ultimate_ft(*ptr);
 	return 0;
 }
 
